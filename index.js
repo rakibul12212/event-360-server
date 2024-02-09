18995d3df6fa1660298b273b2c1fabd5a07ca3ac
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-10 04:26:39 +0600

    uri updated

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xbwnors.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = process.env.DB_URI;
-console.log(uri);
+const uri = process.env.Mongouri;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
