4782437168e9508e0e9d10b6eb130120d34dba31
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-02-11 03:52:07 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
+
 const port = process.env.PORT || 5000;
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xbwnors.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.DB_URI;
+console.log(uri);
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xbwnors.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = process.env.DB_URI;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
